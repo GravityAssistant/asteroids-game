1b288a9bed7fd4719982792d6b0c7b6a3288992b
Malte Sperling <malte_sperling@gmx.de>
2025-06-01 11:40:11 +0200

    change player impl

--- player.py
+++ player.py
@@ -18,14 +18,13 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-    
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        # a = turn left
         if keys[pygame.K_a]:
-            self.rotate(dt * -1)
+            self.rotation -= PLAYER_TURN_SPEED * dt
+        # d = turn right
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotation += PLAYER_TURN_SPEED * dt
